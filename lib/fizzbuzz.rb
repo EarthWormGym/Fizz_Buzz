4824c12748285a034a8af940a4ebad158ffd345f
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-09-23 15:56:00 +0100

    Actual Fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,10 @@
-
 def fizzbuzz(number)
-  'fizz'
+  if number % 5 == 0 && number % 3 == 0
+    number = "fizzbuzz"
+  elsif number % 5 == 0
+    number = "buzz"
+  elsif number % 3 == 0
+    number = "fizz"
+  end
+  puts number
 end
