0c470d17d2ac2f8a702374cfc93761ca2377244c
adam.beresford <adam56.beresford@gmail.com>
2019-09-23 16:16:31 +0100

    alternate rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,13 @@
 
-  i = 0
-  puts "running"
-  for i in 1..100 do
-    if i % 5 == 0 && i % 3 == 0
-      i = "fizzbuzz"
-    elsif i % 5 == 0
-      i = "buzz"
-    elsif i % 3 == 0
-      i = "fizz"
-    end
-  puts i
+i = 0
+puts "running"
+for i in 1..100 do
+  if i % 5 == 0 && i % 3 == 0
+    i = "fizzbuzz"
+  elsif i % 5 == 0
+    i = "buzz"
+  elsif i % 3 == 0
+    i = "fizz"
   end
+puts i
+end
